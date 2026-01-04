d46a80b12b6b545a3f67ae531d7b674fda8350e1
luiseduardoLM <luiseduardo@ciencias.unam.mx>
2026-01-04 13:55:16 -0600

    Estructura de la pagina

--- Portfolio/portfolio.py
+++ Portfolio/portfolio.py
@@ -1,2 +1,8 @@
 import streamlit as st
 
+layout = "centered"
+page_title = "Portfolio | Luis Eduardo Martínez"
+page_icon = ""
+
+#Config de la pagina
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
